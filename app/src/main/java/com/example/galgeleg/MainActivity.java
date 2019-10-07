bb4bca81b9267ddca564a31756e21c1239dae770
Anders <s185100@student.dtu.dk>
2019-10-07 22:03:59 +0200

    Skelet til layout.

--- app/src/main/java/com/example/galgeleg/MainActivity.java
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Galgelogik logik = new Galgelogik();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
